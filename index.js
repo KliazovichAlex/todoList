6e750b1cb8e259578f2838d774b755f50f4053a2
Kliazovich Alex <aliaksandrkliazovich@gamil.com>
2020-11-09 16:55:55 +0300

    upgrade toDo

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-//modal
+// btn to create a new card
 const addCard = document.querySelectorAll(".card_fieldAdd");
-const submitBtn = document.querySelector(".subBtn");
+
+//modal
 const modalWrapper = document.querySelector(".modalWrapper");
 const form = document.querySelector("#form");
+const submitBtn = document.querySelector(".subBtn");
 const task = document.querySelector("#task");
 const description = document.querySelector("#description");
-const cardField = document.querySelector(".card_field");
-let data = [];
+
+//main wrapper
+const wrapper = document.querySelector(".wrapper");
+// const cardField = document.querySelector(".card_field");
+const toDo = document.querySelector(".toDo");
+const progress = document.querySelector(".inProgress");
+const done = document.querySelector(".done");
+const trash = document.querySelector(".deleted");
+
+let data = [
+  (todoArr = []),
+  (progressArr = []),
+  (doneArr = []),
+  (trashArr = []),
+];
 
 addCard.forEach((adder) => {
   adder.addEventListener("click", function (event) {
@@ -18,64 +33,93 @@ addCard.forEach((adder) => {
 form.addEventListener("submit", function (event) {
   modalWrapper.classList.toggle("closeModal");
   event.preventDefault();
-  data.push({
+  data[0].push({
     title: task.value,
     description: description.value,
   });
-  console.log(data);
-  write();
+  drawACard(toDo, 0);
 });
 
-function write() {
-  cardField.innerHTML = "";
-  data.forEach((element) => {
-    cardField.innerHTML += `<div class="bk">
+function drawACard(card, marker) {
+  let desk = card.querySelector(".card_field");
+  desk.innerHTML = "";
+  data[marker].forEach((element) => {
+    desk.innerHTML += `<div class="task">
     <h1 id="one">${element.title}</h1>
     <p id="two">${element.description}</p> 
     <div id="red"></div>
-    <div id="del"><div>
+    <div id="del"></div>
+    <div id="prog"></div>
     </div>
     `;
   });
 }
-//   const first = cardField.firstChild;
-//   cardField.insertBefore(newDiv, first);
-// }
 
-function clearBoard(val, brd) {
-  data.forEach((elemetn, index) => {
-    if (elemetn.title === val) {
-      data.splice(index, 1);
-      brd.innerHTML = "";
-      write();
+wrapper.addEventListener("click", (event) => {
+  const task = event.target.closest(".task");
+  console.log(task);
+  if (event.target === task.querySelector("#del")) {
+    deleteACard(task);
+    console.log("del");
+  }
+  if (event.target === task.querySelector("#red")) {
+    console.log("red");
+    reductACard(task);
+  }
+  if (event.target === task.querySelector("#prog")) {
+    console.log("prog");
+    cardInProgress(task);
+  }
+});
+
+function deleteACard(card) {
+  const cardTitle = card.querySelector("#one").textContent;
+  data[numberOfDirictory(card)].forEach((element, index) => {
+    if (element.title === cardTitle) {
+      const curentCardValue = data[numberOfDirictory(card)].splice(index, 1);
+      console.log(numberOfDirictory(card));
+      data[3].push(curentCardValue[0]);
+      drawACard(toDo, 0);
+      drawACard(progress, 1);
+      drawACard(done, 2);
+      drawACard(trash, 3);
     }
   });
 }
 
-cardField.addEventListener("click", (event) => {
-  const board = event.target.closest(".bk");
-  const titleText = board.querySelector("#one").textContent;
-  const reductText = board.querySelector("#two").textContent;
-  console.log(reductText);
-  if (event.target.closest("#del")) {
-    clearBoard(titleText, cardField);
-  } else if (event.target.closest("#red")) {
-    cardField.innerHTML = "";
-    reductBoard(titleText, reductText);
-  }
-});
-
-function reductBoard(t, d) {
+function reductACard(card) {
   modalWrapper.classList.toggle("closeModal");
-  task.value = t;
-  description.value = d;
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    data.forEach((element, index) => {
-      if (element.description == d) {
-        data.splice(index, 1);
-        write();
-      }
-    });
+  data[numberOfDirictory(card)].forEach((element, index) => {
+    if (element.title === card.querySelector("#one").textContent) {
+      task.value = element.title;
+      description.value = element.description;
+      data[numberOfDirictory(card)].splice(index, 1);
+    }
   });
+}
+
+function cardInProgress(card) {
+  const cardTitle = card.querySelector("#one").textContent;
+  data[numberOfDirictory(card)].forEach((element, index) => {
+    if (element.title === cardTitle) {
+      const curentCardValue = data[numberOfDirictory(card)].splice(index, 1);
+      data[numberOfDirictory(card) + 1].push(curentCardValue[0]);
+      drawACard(toDo, 0);
+      drawACard(progress, 1);
+      drawACard(done, 2);
+      drawACard(trash, 3);
+    }
+  });
+}
+
+function numberOfDirictory(unknown) {
+  if (unknown.closest(".toDo") === toDo) {
+    return 0;
+  } else if (unknown.closest(".inProgress") === progress) {
+    return 1;
+  } else if (unknown.closest(".done") === done) {
+    return 2;
+  } else if (unknown.closest(".deleted") === trash) {
+    return 3;
+  }
 }
